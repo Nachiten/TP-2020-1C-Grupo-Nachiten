e53a97c691672153a8e777f39cec6b0431148860
Ignacio Baptista [Lubuntu] <ibaptista@est.frba.utn.edu.ar>
2020-04-26 11:44:26 -0300

    Creados 4 proyectos con.c de prueba (Team,Broker,GameBoy,GameCard)

---
+++ GameCard/src/gamecard.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h> // Para tener el uint32_t
+
+int main(void) {
+
+	int i = 1;
+	printf("Probando GameCard %i", i);
+
+	return 0;
+}
---
+++ Team/src/team.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h> // Para tener el uint32_t
+
+int main(void) {
+
+	int i = 1;
+	printf("Probando team %i", i);
+
+	return 0;
+}

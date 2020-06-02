23c32fb74d615dcb42f7df20e5bcfa78f3bbc594
Ignacio Baptista [Notebook] <ibaptista@est.frba.utn.edu.ar>
2020-06-02 19:12:29 -0300

    Resuelto conflict de socket.h

--- shared/shared/socket.h
+++ shared/shared/socket.h
@@ -55,16 +55,6 @@ typedef struct
 
 pthread_t thread;
 
-<<<<<<< HEAD
-uint32_t establecer_conexion(char* ip, char* puerto);//se le da una IP y un PUERTO y establece la conexion
-//dice el resultado de intentar conectarse a un modulo
-void resultado_de_conexion(uint32_t socket, t_log* logger, char* modulo); //enviarle el socket al que se intenta conectar, el logger y nombre del modulo al que intentamos conectar.
-void cerrar_conexion(uint32_t socket); //se le da el socket y lo libera
-void mandar_mensaje(void* mensaje, codigo_operacion tipoMensaje,  uint32_t socket_cliente); //se le da el mensaje, el tipo de mensaje que vamos a mandar y el socket que le dice donde mandarlo
-//se le da el paquete, el mensaje, que clase de mensaje es, y un int para que "anote" cuanto pesa el paquete despues de serializarlo. y deja listo para mandar
-void* serializar_paquete(t_paquete* paquete, void* mensaje, codigo_operacion tipoMensaje, uint32_t *size_serializado);
-char* recibir_mensaje(uint32_t socket_cliente, uint32_t* size); //recibe un mensaje
-=======
 //los socket tienen que ser INT
 
 uint32_t establecer_conexion(char* ip, char* puerto);//se le da una IP y un PUERTO y establece la conexion
@@ -75,7 +65,6 @@ void mandar_mensaje(void* mensaje, codigo_operacion tipoMensaje, int socket_clie
 //se le da el paquete, el mensaje, que clase de mensaje es, y un int para que "anote" cuanto pesa el paquete despues de serializarlo. y deja listo para mandar
 void* serializar_paquete(t_paquete* paquete, void* mensaje, codigo_operacion tipoMensaje, uint32_t *size_serializado);
 char* recibir_mensaje(int socket_cliente, uint32_t* size); //recibe un mensaje
->>>>>>> broker_en_proceso
 void eliminar_paquete(t_paquete* paquete); //libera la memoria utilizada para manejar los paquetes
 
 //serializar en prueba

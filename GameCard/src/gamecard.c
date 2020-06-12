de2a3723e4a2f1c9c0b00535fd53ab91fdf91bc1
Ignacio Baptista [Lubuntu] <ibaptista@est.frba.utn.edu.ar>
2020-06-12 19:18:36 -0300

    Funcionando crear log y leer config correcto

--- GameCard/src/gamecard.c
+++ GameCard/src/gamecard.c
@@ -1,9 +1,30 @@
-#include "GameCard.h"
+#include "gamecard.h"
 
 int main(void) {
 
-	int i = 1;
-	printf("Probando GameCard %i", i);
+	t_log* logger;
+	t_config* config;
+	char* ip;
+	char* puerto;
+	config = leerConfiguracion("/home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/Configs/GameCard.config");
+
+	logger = cargarUnLog("/home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/GameCard/Logs/GameCard.log", "GAMECARD");
+
+	ip = config_get_string_value(config, "IP_BROKER");
+	if (ip == NULL){
+		log_error(logger, "No se ha podido leer la 'ip' de la config");
+		return 3;
+	} else {
+		log_info(logger, "Se leyo la ip del broker correctamente");
+	}
+	puerto = config_get_string_value(config, "PUERTO_BROKER");
+	if (puerto == NULL){
+		log_error(logger, "No se ha podido leer el 'puerto' de la config");
+		return 3;
+	} else {
+		log_info(logger, "Se leyo el puerto del broker correctamente");
+	}
+
 
 	return EXIT_SUCCESS;
 }

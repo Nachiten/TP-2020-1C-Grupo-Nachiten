524b4790002b553c85ec3265de557a4ac2508088
Ignacio Baptista [Lubuntu] <ibaptista@est.frba.utn.edu.ar>
2020-08-05 00:18:20 -0300

    Fixeado carpeta debug de gameboy

---
+++ GameBoy/Debug/src/gameboy.d
@@ -0,0 +1,27 @@
+src/gameboy.o: ../src/gameboy.c ../src/GameBoy.h \
+ /home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/socket.h \
+ /home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/estructuras.h \
+ /home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/utilidades.h \
+ /home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/cargador.h \
+ /home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/terminarPrograma.h \
+ /home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/socket.h \
+ /home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/estructuras.h \
+ /home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/utilidades.h
+
+../src/GameBoy.h:
+
+/home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/socket.h:
+
+/home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/estructuras.h:
+
+/home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/utilidades.h:
+
+/home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/cargador.h:
+
+/home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/terminarPrograma.h:
+
+/home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/socket.h:
+
+/home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/estructuras.h:
+
+/home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared/shared/utilidades.h:
--- GameBoy/Debug/src/subdir.mk
+++ GameBoy/Debug/src/subdir.mk
@@ -4,32 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../shared/cargador.c \
-../shared/memoria.c \
-../shared/socket.c \
-../shared/terminarPrograma.c \
-../shared/utilidades.c 
+../src/gameboy.c 
 
 OBJS += \
-./shared/cargador.o \
-./shared/memoria.o \
-./shared/socket.o \
-./shared/terminarPrograma.o \
-./shared/utilidades.o 
+./src/gameboy.o 
 
 C_DEPS += \
-./shared/cargador.d \
-./shared/memoria.d \
-./shared/socket.d \
-./shared/terminarPrograma.d \
-./shared/utilidades.d 
+./src/gameboy.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-shared/%.o: ../shared/%.c
+src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -I"/home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/shared" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 

f79a3b76233ab0708eb5ebe714781475115d466b
TheLucasPrada <lucas@roprada.com.ar>
2020-08-08 11:41:34 -0300

    arreglando conflicts

--- correrTeam1Final.sh
+++ correrTeam1Final.sh
@@ -4,10 +4,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/workspace/tp-2020-1c-Grupo-Nachiten/sh
 echo [Moviendose a carpeta de Team...]
 sleep 1
 cd Team/Debug
-echo [Compilando...]
-sleep 1
-make clean
-make all
 echo [Ejecutando Team...]
 sleep 1
 ./Team "/home/utnso/workspace/tp-2020-1c-Grupo-Nachiten/Configs/Team1Final.config"
